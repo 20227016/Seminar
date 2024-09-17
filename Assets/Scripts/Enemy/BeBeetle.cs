a10073d3c85acf3f5afe779c23441894e857ac7b
STD\s20227075 <20227075-takahashikoei@hcs.ac.jp>
2024-09-17 13:39:56 +0900

    蚊のエネミー

--- Assets/Scripts/Enemy/BeBeetle.cs
+++ Assets/Scripts/Enemy/BeBeetle.cs
@@ -18,6 +18,8 @@ public class BeBeetle : BaseEnemy
     [SerializeField, Tooltip("プレイヤーオブジェクト")]
     private GameObject _player = default;
 
+    [SerializeField]
+    private Animator _animator = default;
 
     private void Start()
     {
@@ -32,7 +34,30 @@ public class BeBeetle : BaseEnemy
     {
         //例キャスト初期設定
         BasicRaycast();
-        _boxCastStruct._distance = 3f;
+
+        // 中心点を取得
+        _boxCastStruct._originPos = this.transform.position;
+
+        // 索敵範囲の距離
+        _boxCastStruct._distance = 1f;
+
+        // 自分のスケール(x)を取得
+        float squareSize = transform.localScale.x;
+
+        // BoxCastを正方形のサイズにする
+        _boxCastStruct._size = new Vector2(squareSize, squareSize);
+
+
+
+        // マウスクリックで攻撃アニメーションに切り替える
+        if (Input.GetMouseButtonDown(0))  // 左クリック
+        {
+            _animator.SetInteger("", 1);  // 攻撃アニメーションに切り替え
+        }
+        else
+        {
+            _animator.SetInteger("", 0);  // アイドルアニメーションに戻す
+        }
     }
 
 }
